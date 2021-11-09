29c374fde1eda3101b9f4c6d7d5940c10e4abb7f
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 05:15:32 +0700

    Update NecessaryIndex.php

--- views/Necessary/NecessaryIndex.php
+++ views/Necessary/NecessaryIndex.php
@@ -1,4 +1,10 @@
-echo("tablenaa")
+<form method="get" action="">
+    <input type="text" name="key">
+    <input type="hidden" name="controller" value="Necessary"/>
+    <button type="submit" name="action" value="search">
+    search</button>
+</form>
+new Necessary<a href="?controller=Necessary&action=newNecessary"> click! </a><br>
 <table border=2>
     <tr> <td> Patient_id </td>
     <td> NamePeople </td>
@@ -12,8 +18,6 @@ echo("tablenaa")
     <td> gel_alcohol </td>
     <td> thermometer </td>
     <td> PulseOximeter </td>
-    <td> PulseOximeter </td>
-    <td> PulseOximeter </td>
     <td>update</td> 
     <td>delete</td></tr>
 
@@ -23,7 +27,7 @@ echo("tablenaa")
         <td> $ness->NamePeople </td>
         <td> $ness->LastnameP </td>
         <td> $ness->address_Home </td>
-        <td> $ness->country_Home </td>
+        <td> $ness->county_Home </td>
         <td> $ness->province_Home </td>
         <td>$ness->smask</td> 
         <td>$ness->garb</td> 
@@ -31,8 +35,8 @@ echo("tablenaa")
         <td>$ness->gel</td> 
         <td>$ness->therm</td> 
         <td>$ness->pulse</td>
-        <td>update</td> 
-        <td>delete</td></tr>";
+        <td><a href=?controller=Necessary&action=updateFormN&pid=$ness->pid>update</td> 
+        <td><a href=?controller=Necessary&action=deleteConfirm&pid=$ness->pid>delete</td></tr>";
     }
     echo "</table>";
     ?>
