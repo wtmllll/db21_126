1cfcaf7a1a30d95932f55641c4fc957699e6210f
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 00:20:14 +0700

    Update necessModel.php

--- models/necessModel.php
+++ models/necessModel.php
@@ -7,16 +7,26 @@ class NecessModel{
     public $gel;
     public $therm;
     public $pulse;
+    public $NamePeople;
+    public $LastnameP; 
+    public $address_Home;
+    public $county_Home;
+    public $province_Home;
 
     public function __construct ($pid,$smask,$garb,$med,$gel,$therm,$pulse)
     {
-        $this->$pid = $pid;
-        $this->$smask = $smask;
-        $this->$garb = $garb;
-        $this->$med = $med;
-        $this->$gel = $gel;
-        $this->$therm = $therm;
-        $this->$pulse = $pulse;
+        $this->pid = $pid;
+        $this->smask = $smask;
+        $this->garb = $garb;
+        $this->med = $med;
+        $this->gel = $gel;
+        $this->therm = $therm;
+        $this->pulse = $pulse;
+        $this->NamePeople = $NamePeople;
+        $this->LastnameP = $LastnameP; 
+        $this->address_Home = $address_Home;
+        $this->county_Home = $county_Home;
+        $this->province_Home = $province_Home;
     }
     public static function getAll()
     {   
@@ -33,7 +43,12 @@ class NecessModel{
             $gel = $my_row[gel_alcohol];
             $therm = $my_row[thermometer];
             $pulse = $my_row[PulseOximeter];
-            $necessList[] = new NessModel($pid, $smask, $garb, $med, $gel, $therm, $pulse);
+            $NamePeople = $my_row[NamePeople];
+            $LastnameP = $my_row[LastnameP]; 
+            $address_Home = $my_row[address_Home];
+            $county_Home = $my_row[county_Home];
+            $province_Home = $myrow[province_Home];
+            $necessList[] = new NessModel($pid, $smask, $garb, $med, $gel, $therm, $pulse,$NamePeople,$LastnameP,$address_Home,$county_Home,$province_Home);
         }
         require("connection_close.php");
         
