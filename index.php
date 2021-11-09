b00737db4ee1fa1a0c38cd9cf4748cbd06eba5fd
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 00:03:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+if(isset($_GET['controller'])&&isset($_GET['action']))
+{
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
+}
+else
+{
+    $controller = 'pages';
+    $action = 'home';
+}
+?>
+
+<html>
+    <head></head>
+    <body>
+    <?php echo "Controller = ".$controller."  |  Action = ".$action;?>
+        <br>[<a href="?controller=pages&action=home">Home</a>]
+        [<a href="?controller=Necessary&action=index">Necessary</a>]
+        <br>
+
+        <?php require_once("routes.php");?>
+    </body>
+</html>
