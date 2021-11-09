97887a1c8db9d64c5bc8d9db97202631e2a6770d
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 05:15:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,9 +15,10 @@ else
     <head></head>
     <body>
     <?php echo "Controller = ".$controller."  |  Action = ".$action;?>
-        <br>[<a href="?controller=pages&action=home">Home</a>]
-        [<a href="?controller=Necessary&action=index">Necessary</a>]
+        <br><a href="?controller=pages&action=home">[Home]</a>
+        <a href="?controller=Necessary&action=index">[Necessary]</a>
         <br>
+        
 
         <?php require_once("routes.php");?>
     </body>
