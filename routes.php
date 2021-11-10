ebc51e176110d03328096b28a38d3cf622f86e88
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 23:24:54 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,16 +1,25 @@
 <?php
-$controllers = array('pages'=>['home','error'],
-                    'Necessary'=>['index','newNecessary','addNecessary','search','updateFormN','updateN','deleteConfirmN','deleteN']);
+$controllers = array('pages'=>['home','error','Nindex','Sindex'],
+                    'Necessary'=>['index','newNecessary','addNecessary','search','updateFormN','updateN','deleteConfirmN','deleteN'],
+                    'Symptoms'=>['index','newSymptoms','addSymptoms','search','updateFormS','updateS','deleteConfirmS','deleteS']
+                    );
 function call($controller,$action) {
     require_once("controllers/".$controller."_controller.php");
     switch($controller)
     {
         case "pages":   $controller = new PagesController();
+                        require_once("models/NecessaryModel.php");
+                        require_once("models/SymptomModel.php");
                         break;
         case "Necessary":   require_once("models/NecessaryModel.php");
                             require_once("models/necessModel.php");
                             require_once("models/HomeIsolationModel.php");
                             $controller = new NessController();
+                            break;
+        case "Symptoms":    require_once("models/SymptomModel.php");
+                            require_once("models/Color.php");
+                            require_once("models/HomeIsolationModel.php");
+                            $controller = new SympController();
                             break;
     }
     $controller->{$action}();
