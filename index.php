84f25f17e48e563e69c71cf4dcfa5a201b911162
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 23:24:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ else
     <?php echo "Controller = ".$controller."  |  Action = ".$action;?>
         <br><a href="?controller=pages&action=home">[Home]</a>
         <a href="?controller=Necessary&action=index">[Necessary]</a>
+        <a href="?controller=Symptoms&action=index">[Symptoms]</a>
         <br>
         
 
