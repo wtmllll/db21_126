cdb1d043c3e8fd37e4794a0580ea36094c72a0a2
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 23:24:34 +0700

    Update Necessary_Controller.php

--- controllers/Necessary_Controller.php
+++ controllers/Necessary_Controller.php
@@ -1,10 +1,12 @@
 
 <?php 
-class NessController {
-    public function index() {
-        echo "indexjaa";
+class NessController 
+{
+    public function index() 
+    {
         $ness_List = NessModel::getAll();
         require_once('views/Necessary/NecessaryIndex.php');
+
     }
 
     public function newNecessary()
@@ -14,7 +16,8 @@ class NessController {
         require_once('views/Necessary/NewNecessary.php');
     }
 
-    public function addNecessary() {
+    public function addNecessary() 
+    {
         $pid = $_GET['Patient_id'];
         $smask = $_GET['surgical_mask'];
         $garb = $_GET['garbage'];
@@ -22,31 +25,30 @@ class NessController {
         $gel = $_GET['gel_alcohol'];
         $therm = $_GET['thermometer'];
         $pulse = $_GET['PulseOximeter'];
-        echo $pid;
         NessModel::Add($pid, $smask, $garb, $med, $gel, $therm, $pulse);
-       echo "say hi!";
+
         NessController::index();
 
     }
 
-    public function search() {
-        echo "sefcon";
+    public function search() 
+    {
         $key=$_GET['key'];
-        echo $key;
         $ness_List=NessModel::search($key);
         
         require_once('views/Necessary/NecessaryIndex.php');
     }
 
-    public function updateFormN() {
+    public function updateFormN() 
+    {
         $pid = $_GET['Patient_id'];
         $ness_List= NessModel::get($pid);
         $Home_List=HomeIsolation::getAll();
         require_once('views/Necessary/updateFormN.php');
     }
 
-    public function updateN() {
-        echo "upN1";
+    public function updateN() 
+    {
         $pid = $_GET['Patient_id'];
         $smask = $_GET['surgical_mask'];
         $garb = $_GET['garbage'];
@@ -59,15 +61,17 @@ class NessController {
         NessController::index();
     }
 
-    public function deleteConfirmN() {
+    public function deleteConfirmN() 
+    {
         $pid = $_GET['Patient_id'];
         $ness_List=NessModel::get($pid);
         require_once('./views/Necessary/deleteConfirm.php');
     }
 
-    public function deleteN() {
+    public function deleteN() 
+    {
         $pid = $_GET['Patient_id'];
-        NessModel::delete($PDid);
+        NessModel::delete($pid);
         NessController::index();
 
     }
