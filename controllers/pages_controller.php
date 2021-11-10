196fbc5ca488cc7b6155ba835d92e8d99876af5e
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 23:24:37 +0700

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -7,5 +7,15 @@ class PagesController
     public function error() {
         require_once('views/pages/error.php');
     }
+
+    public function Nindex() {
+        $ness_List = NessModel::getAll();
+        require_once('views/pages/homeIndex.php');
+    }
+
+    public function Sindex() {
+        $symptom_list = SympModel::getAll();
+        require_once('views/pages/sympIndex.php');
+    }
 }
 ?>
