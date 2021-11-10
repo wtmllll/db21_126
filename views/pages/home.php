3950bd2d7190d5ada9e26d7b09be685002dd73d5
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-10 23:34:05 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,4 @@
-<p> Welcome to our homepage </p>
+<p> Welcome to our homepage </p><br>
+
+<a href="?controller=pages&action=Nindex">[สิ่งของจำเป็นสำหรับ Home Isolation]</a><br>
+<br><a href="?controller=pages&action=Sindex">[ข้อมูลการ Video Call]</a>
