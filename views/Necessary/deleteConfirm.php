eebc14ad33f93289f43f231dd28ff812e2049c29
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-11 03:47:15 +0700

    Update deleteConfirm.php

--- views/Necessary/deleteConfirm.php
+++ views/Necessary/deleteConfirm.php
@@ -1,6 +1,9 @@
-<?php echo "<br>Are you sure to delete this id?<br>
-            <br>Patient_id: $ness_List->pid";?>
-<form method="get" action="">
+<br><?php echo "<br>Do you want to delete this patient?<br>
+            Patient_id : $ness_List->pid 
+            <br>name : $ness_List->NamePeople   $ness_List->LastnameP"
+            
+            ;?><br>
+<br><form method="get" action="">
     <input type="hidden" name="controller" value="Necessary"/>
     <input type="hidden" name="Patient_id" value="<?php echo $ness_List->pid;?>"/>
     <button type="submit" name="action" value="index"> Back </button>
