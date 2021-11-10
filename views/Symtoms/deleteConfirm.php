d131d1b34222f2c20df908ee8d87ed78a92c0e1e
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-11-11 03:47:30 +0700

    Update deleteConfirm.php

--- views/Symtoms/deleteConfirm.php
+++ views/Symtoms/deleteConfirm.php
@@ -1,6 +1,8 @@
-<?php echo "<br>Are you sure to delete this id?<br>
-            <br>Patient_id: $symptom_list->pid";?>
-<form method="get" action="">
+<br><?php echo "<br>Are you sure to delete this patient?<br>
+            <br>Patient_id: $symptom_list->pid
+            <br>Name : $symptom_list->name   $symptom_list->lastname"
+            ;?><br>
+<br><form method="get" action="">
     <input type="hidden" name="controller" value="Symptoms"/>
     <input type="hidden" name="Patient_id" value="<?php echo $symptom_list->pid;?>"/>
     <button type="submit" name="action" value="index"> Back </button>
